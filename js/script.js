b3cabcf800f59aaf993baee8e3fa61b8cddfe2e2
fcarrara24 <f.carrara24@studenti.unibg.it>
2023-10-27 15:42:18 +0200

    BONUS: added image to folder, trying to refresh it

--- js/script.js
+++ js/script.js
@@ -5,7 +5,18 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            message: 'message to print'
+            message: 'message to print',
+            imgLink: 'https://picsum.photos/200/300'
+        };
+    },
+    methods: {
+        newImage() {
+            console.time('loadpic')
+            console.log('hi')
+            //necessary to refresh link from lorem picsum
+            this.imgLink = 'https://picsum.photos/200/400'
+            console.timeEnd('loadpic')
+
         }
     }
 }).mount('#app');
