039b13739fcf72181f6cedf49c373fe59284b9c1
fcarrara24 <f.carrara24@studenti.unibg.it>
2023-10-27 16:27:33 +0200

    added a spinning animation to the image

--- js/script.js
+++ js/script.js
@@ -6,17 +6,24 @@ createApp({
     data() {
         return {
             message: 'title via vue',
-            imgLink: 'https://picsum.photos/200/400'
+            imgLink: 'https://picsum.photos/200/400',
+            flip: 'flippable'
 
         };
     },
     methods: {
         newImage() {
+
             console.time('loadpic')
             console.log('hi')
             //using a flip flop-like method to ensure img refresh
-            this.imgLink = this.imgLink === 'https://picsum.photos/200/400' ? 'https://picsum.photos/200/401' : 'https://picsum.photos/200/400'
+            this.flip = this.flip === 'flippable' ? 'antiFlippable' : 'flippable';
             console.timeEnd('loadpic')
+
+            setTimeout(() => {
+                this.imgLink = this.imgLink === 'https://picsum.photos/200/400' ? 'https://picsum.photos/200/401' : 'https://picsum.photos/200/400'
+
+            }, 400)
 
         }
     }
